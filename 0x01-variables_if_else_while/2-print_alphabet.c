41e06079a088e2148385e4873d35d2f15466779c
justice odoh <justiceodoh@gmail.com>
2023-06-27 00:21:27 -0700

    to lowercase code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - turning to lowercase
+ *
+ * Return: always return (0)
+*/
+
+int main(void)
+{
+	int x = 97;
+
+	while (x < 123)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
